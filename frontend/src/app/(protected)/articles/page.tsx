5cbc1d12318ea22008a2b073ade3943bfa754ee8
Alejo Borracci <129368174+alejoborracci21@users.noreply.github.com>
2025-05-02 00:23:03 -0300

    routes

---
+++ frontend/src/app/(protected)/articles/page.tsx
@@ -0,0 +1,8 @@
+export default function Articles() {
+    return (
+      <div>
+        <h1>Articles Page</h1>
+        <p>Welcome to the Articles!</p>
+      </div>
+    );
+  }
---
+++ frontend/src/app/(protected)/generator/page.tsx
@@ -0,0 +1,8 @@
+export default function Generator() {
+    return (
+      <div>
+        <h1>Generator Page</h1>
+        <p>Welcome to the Generator!</p>
+      </div>
+    );
+  }
