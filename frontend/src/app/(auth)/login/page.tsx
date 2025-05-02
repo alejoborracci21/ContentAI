64b432fb009ad1e0c89d6ffc68944a1911d4283e
Alejo Borracci <129368174+alejoborracci21@users.noreply.github.com>
2025-05-02 17:50:17 -0300

    login branch

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,8 +1,30 @@
+'use client';
+
 export default function Login() {
-    return (
-      <div>
-        <h1>Login Page</h1>
-        <p>Welcome to the Login!</p>
-      </div>
-    );
-  }
+
+  const onSubmit = (formData: FormData) => {
+    const username = formData.get("username");  
+    const password = formData.get("password");
+    console.log("Email or Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-4 items-center justify-center bg-slate-100 p-4 rounded-md shadow-md text-black"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        onSubmit(formData);
+      }}
+    >
+      <label htmlFor="username">Email or Username:</label>
+      <input className="border" type="text" id="username" name="username" required />
+      <br />
+      <label htmlFor="password">Password:</label>
+      <input className="border" type="password" id="password" name="password" required />
+      <br />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
