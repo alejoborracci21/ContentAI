bb4867d1a436d5e356fb7bdc4db2491a08c5bb38
ravenbbs <julioc3bgud@gmail.com>
2025-05-05 17:30:39 -0500

    Add themechanger on app

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,15 +1,58 @@
-"use client"
+"use client";
 
-import { Sidebar } from "@/components/Sidebar"
-import ClientAuthWrapper from "@/components/ClientAuthWrapper"
+import { AppSidebar } from "@/components/AppSidebar";
+import ClientAuthWrapper from "@/components/ClientAuthWrapper";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Switch } from "@/components/ui/switch";
+import { MoonIcon, SunMediumIcon } from "lucide-react";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
-export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+import "../globals.css";
+
+export default function ProtectedLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  //Theme changer
+  const { theme, setTheme } = useTheme();
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    setIsDarkMode(theme === "dark");
+  }, [theme]);
+
+  const handleToggle = (checked: boolean) => {
+    setIsDarkMode(checked);
+    setTheme(checked ? "dark" : "light");
+  };
+
   return (
     <ClientAuthWrapper>
-      <div className="flex min-h-screen">
-        <Sidebar />
-        <main className="flex-1 p-6">{children}</main>
-      </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <main className="px-6 pt-2 w-full">
+          <div className="flex items-center justify-between mb-2">
+            <SidebarTrigger />
+            <Switch
+              id="theme-mode"
+              checked={isDarkMode}
+              onCheckedChange={handleToggle}
+              thumbClassName="h-7 w-7 data-[state=checked]:translate-x-6"
+              className="h-8 w-14 ml-auto"
+              icon={
+                isDarkMode ? (
+                  <MoonIcon className="h-4 w-4" />
+                ) : (
+                  <SunMediumIcon className="h-4 w-4" />
+                )
+              }
+            />
+          </div>
+          {children}
+        </main>
+      </SidebarProvider>
     </ClientAuthWrapper>
-  )
+  );
 }
