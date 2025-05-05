167a351ae3fa0ed413437b60d96b053130f162ce
ravenbbs <julioc3bgud@gmail.com>
2025-05-05 14:35:14 -0500

    Add bg for light and dark mode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={` ${geistSans.variable} ${geistMono.variable} `}>
-        <ThemeProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         {children}
         </ThemeProvider>
       </body>
