b3369b1d2e7f46ec82bed2b7751c4df1538b55cd
ravenbbs <julioc3bgud@gmail.com>
2025-05-05 21:15:09 -0500

    Add styles

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,90 +1,130 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/lib/firebase';
-import { Card, CardContent } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function LoginPage() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { toast } = useToast();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
-    
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    const token = await user.getIdToken();
-    localStorage.setItem('token', token);
+    setLoading(true);
 
     try {
-      if(!token) throw new Error('El usuario no existe o no tiene token');
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      const token = await user.getIdToken();
 
-      alert('Login exitoso');
-      router.push('/articles');
+      if (!token) throw new Error("Token inválido");
+
+      localStorage.setItem("token", token);
+      toast({
+        title: "Inicio de sesión exitoso",
+        description: "Redirigiendo a artículos...",
+        variant: "blue",
+      });
+
+      router.push("/articles");
     } catch (error) {
-      alert('Error: ' + (error as Error).message);
+      toast({
+        title: "Error al iniciar sesión",
+        description: (error as Error).message,
+        variant: "destructive",
+      });
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
-      <Card className="w-full max-w-md p-6">
-        <CardContent>
-          <h1 className="text-2xl font-bold mb-6 text-center">Iniciar sesión</h1>
-          <form onSubmit={handleLogin} className="space-y-4">
-            <div>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="correo@ejemplo.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
+    <main className="flex min-h-screen items-center justify-center px-4">
+      <div className="absolute top-0 -z-10 h-full w-full ">
+        <div className="absolute left-1/2  h-[500px] w-[500px] -translate-x-[50%] translate-y-[50%] rounded-full bg-[rgba(90,128,252,0.47)] opacity-60 blur-[180px]"></div>
+      </div>
+      <Toaster />
+      <div className="flex flex-col gap-2 w-full max-w-md">
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Bienvenido de nuevo</CardTitle>
+            <CardDescription>Inicia sesión con tu cuenta</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleLogin} className="grid gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="correo@ejemplo.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Contraseña</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full" disabled={loading}>
+                {loading ? "Iniciando..." : "Iniciar sesión"}
+              </Button>
+            </form>
+            <div className="text-center text-sm mt-4">
+              ¿No tenés cuenta?{" "}
+              <button
+                onClick={() => router.push("/register")}
+                className="underline text-blue-600 hover:text-blue-800"
+              >
+                Registrate
+              </button>
             </div>
-            <div>
-              <Label htmlFor="password">Contraseña</Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+            <div className="mt-2 text-center">
+              <Button
+                variant="outline"
+                className="w-full mt-2"
+                onClick={() => router.push("/")}
+              >
+                Volver al inicio
+              </Button>
             </div>
-            <Button type="submit" className="w-full">
-              Iniciar sesión
-            </Button>
-          </form>
-          <div className="mt-4 text-sm text-center">
-            ¿No tenés cuenta?{' '}
-            <button
-              type="button"
-              className="text-blue-600 underline hover:text-blue-800"
-              onClick={() => router.push('/register')}
-            >
-              Registrate
-            </button>
-          </div>
-          <div className="mt-2 text-sm text-center">
-            <Button
-              variant="outline"
-              className="w-full mt-2"
-              onClick={() => router.push('/')}
-            >
-            Volver al inicio
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+          By clicking continue, you agree to our{" "}
+          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+        </div>
+      </div>
     </main>
   );
 }
--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -1,115 +1,150 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '@/lib/firebase';
-import { Card, CardContent } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Button } from '@/components/ui/button';
-import { createUserInBackend } from '@/lib/api/users';
-import { useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { createUserInBackend } from "@/lib/api/users";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function RegisterPage() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const { toast } = useToast();
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  
-      useEffect(() => {
-          const token = localStorage.getItem('token');
-          if (token) {
-            router.push('/articles');
-          }
-        }, [router]);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      router.push("/articles");
+    }
+  }, [router]);
 
   const handleRegister = async (e: React.FormEvent) => {
     e.preventDefault();
-
+    setLoading(true);
 
     try {
       await createUserWithEmailAndPassword(auth, email, password);
       const token = await auth.currentUser?.getIdToken();
 
       if (token) {
-        createUserInBackend(token, {
+        await createUserInBackend(token, {
           nombre: name,
         });
+        toast({
+          title: "Registro exitoso",
+          description: "Redirigiendo al login...",
+          variant:"blue"
+        });
+        router.push("/login");
+      } else {
+        throw new Error("No se pudo obtener el token");
       }
-      alert('Registro exitoso');
-      router.push('/login');
     } catch (error) {
-      alert('Error: ' + (error as Error).message);
+      toast({
+        title: "Error al registrarse",
+        description: (error as Error).message,
+        variant: "destructive",
+      });
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100 px-4">
-      <Card className="w-full max-w-md p-6">
-        <CardContent>
-          <h1 className="text-2xl font-bold mb-6 text-center">Crear cuenta</h1>
-          <form onSubmit={handleRegister} className="space-y-4">
-            <div>
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="correo@ejemplo.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div>
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                type="name"
-                placeholder="Juan Perez"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
+    <main className="flex min-h-screen items-center justify-center px-4">
+      <div className="absolute top-0 -z-10 h-full w-full "><div className="absolute left-1/2  h-[500px] w-[500px] -translate-x-[50%] translate-y-[50%] rounded-full bg-[rgba(90,128,252,0.47)] opacity-60 blur-[180px]"></div></div>
+      <Toaster />
+      <div className="flex flex-col gap-2 w-full max-w-md">
+        <Card>
+          <CardHeader className="text-center">
+            <CardTitle className="text-xl">Crear cuenta</CardTitle>
+            <CardDescription>
+              Completá los campos para registrarte
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleRegister} className="grid gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="correo@ejemplo.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="name">Nombre completo</Label>
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="Juan Pérez"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Contraseña</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full" disabled={loading}>
+                {loading ? "Registrando..." : "Registrarse"}
+              </Button>
+            </form>
+
+            <div className="mt-4 text-sm text-center">
+              ¿Ya tenés cuenta?{" "}
+              <button
+                type="button"
+                className="text-blue-600 underline hover:text-blue-800"
+                onClick={() => router.push("/login")}
+              >
+                Iniciar sesión
+              </button>
             </div>
 
-            <div>
-              <Label htmlFor="password">Contraseña</Label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="********"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
+            <div className="mt-2">
+              <Button
+                variant="outline"
+                className="w-full mt-2"
+                onClick={() => router.push("/")}
+              >
+                Volver al inicio
+              </Button>
             </div>
-            <Button type="submit" className="w-full">
-              Registrarse
-            </Button>
-          </form>
-          <div className="mt-4 text-sm text-center">
-            ¿Ya tenés cuenta?{" "}
-            <button
-              type="button"
-              className="text-blue-600 underline hover:text-blue-800"
-              onClick={() => router.push("/login")}
-            >
-              Iniciar sesión
-            </button>
-          </div>
-          <div className="mt-2 text-sm text-center">
-            <Button
-              variant="outline"
-              className="w-full mt-2"
-              onClick={() => router.push("/")}
-            >
-              Volver al inicio
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
+          </CardContent>
+        </Card>
+        <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        and <a href="#">Privacy Policy</a>.
+      </div>
+      </div>
     </main>
   );
 }
